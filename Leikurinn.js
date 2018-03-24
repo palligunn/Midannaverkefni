7cd65902df467b905c6cf5c20905452b22beb586
palligunn <palligunn21@gmail.com>
2018-03-24 15:04:04 +0000

    Þjappa kóðann

--- Leikurinn.js
+++ Leikurinn.js
@@ -9,26 +9,16 @@ function preload() {//hleður inn myndirnar sem verða tilbúnar í notkun
 }
 
 function create() {//byggir leikinn með að setja inn allar myndir sem þú kallaðir á
-	game.physics.startSystem(Phaser.Physics.ARCADE);//setur inn Arcade physics frá phaser
-
+	game.physics.startSystem(Phaser.Physics.ARCADE);//þetta hleður inn myndum af bakgrunninum og hvert það á að fara í leiknum, ásamt að segja því hvernig þetta á að haga sér.
 	game.add.sprite(0, 0, 'sky');//setur inn myndina 'sky' á 0,0
-
 	platforms = game.add.group();//setur inn platforma fyrir jörðina
-
 	platforms.enableBody = true;//setur physics á platform objectið
-
 	var ground = platforms.create(0, game.world.height - 64, 'ground');//býr til jörðina og skýrir það 'ground'
-
 	ground.scale.setTo(2,2);//stækkar myndina til þess að hún passi í leikinn
-
 	ground.body.immovable = true;//passar það að jörðinn detti ekki úr ramma eða færist
-
-	var ledge = platforms.create(400, 400, 'ground');//setur inn tvo aðra platforma og notar ground fyrir það
-
+	var ledge = platforms.create(100, 400, 'ground');//setur inn tvo aðra platforma og notar ground fyrir það
 	ledge.body.immovable = true;//gerir það sama fyrir platformana og fyrir jörðina
-
 	ledge = platforms.create(-150, 250, 'ground');
-
 	ledge.body.immovable = true;
 }
 
